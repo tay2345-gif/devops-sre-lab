ec09078d31e0a5a00770ce9cab62e851802aa9af
tay2345-gif <tashuanlawrence7@gmail.com>
2026-03-24 14:57:09 -0400

    rds

---
+++ rds.tf
@@ -0,0 +1,16 @@
+resource "aws_db_subnet_group" "db_subnets" {
+  name       = "db_subnet_group"
+  subnet_ids = [aws_subnet.private.id]
+}
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = var.db_username
+  password             = var.db_password
+  skip_final_snapshot  = true
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.db_subnets.name
+}
